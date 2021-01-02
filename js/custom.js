150733c1f31dc2f20ccafb33b441408fe7c1c63a
Ole Walberg <greblavelo@gmail.com>
2021-01-02 18:11:28 +0100

    Some changes to custom.js have been made, but i dont know what

--- js/custom.js
+++ js/custom.js
@@ -13,29 +13,3 @@ $(window).on("load",function(){
     $(".loader-container").removeClass("d-flex");
   });
 });
-
-/*
-$body = $("body");
-
-$(document).on({
-  ajaxStart: function(){$body.addClass("loading");},
-  ajaxStop: function(){$body.removeClass("loading");}
-});
-
-*/
-
-/*
-This code was an attempt at creating a loader using only
-javascript. But since I couldn't find helping resources
-I ended up using jQuery insted.
-
-let count;
-function loader(){
-  console.log('Lets load this shit.');
-  let elem = document.getElementsByTagName("img");
-  let clo = elem[1].cloneNode(true);
-  console.log(typeof elem);
-
-  document.getElementById("app").insertAdjacentElement("beforeend", clo);
-};
-*/
