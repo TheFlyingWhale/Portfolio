b7b6181bc9943979fa5a0ac89fb1fa4d6b0dea67
Ole Walberg <greblavelo@gmail.com>
2021-01-24 16:02:05 +0100

    refactored a little

--- components/navbar.php
+++ components/navbar.php
@@ -2,8 +2,6 @@
   <div class="container p-0 viewBorder">
     <div class="navbar my-3 viewBorder">
       <a id="homeLink" class="viewBorder color-fg" href="index.php">Ole Walberg</a>
-      <div class="">
-        <?php include 'colorModeToggler.php' ?>
-      </div>
+      <?php include 'colorModeToggler.php' ?>
     </div>
 </nav>
