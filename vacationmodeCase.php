550b62332fba4abae5d59d751042b403584fafc0
Ole Walberg <greblavelo@gmail.com>
2021-01-25 14:10:17 +0100

    fixed vacation mode case

--- vacationmodeCase.php
+++ vacationmodeCase.php
@@ -1,14 +1,15 @@
 <?php include 'head.php' ?>
+
 <body class="color-bg">
-  <?php include 'components/navbar.php'?>
-  <div class="container">
+  <?php include 'components/navbar.php' ?>
+  <div class="container p-0">
     <div class="row p-0">
       <div class="col-lg-6 col-sm-12 greeterCol greeterNoWrap responsiveAlign p-0">
         <h1 class="color-fg">Same old song</h1>
         <h3 class="color-fg">Illustrasjon & Grafisk Design</h3>
         <p class="responsiveInfoText color-fg">
           Albumcover for gruppen Vacation Mode.
-        <br><br>
+          <br><br>
           Du finner dem på spotify!
         </p>
       </div>
@@ -16,19 +17,21 @@
         <img src="content/vacationMode/sameOldSongLP.png" class="img-fluid" style="right:0; width:140%; z-index:-1;" alt="">
       </div>
     </div>
-  </div>
-  <div class="row d-flex justify-content-center spacer150T">
-    <div class="col-lg-8 col-md-12">
-      <img src="content/vacationMode/vacationMode.jpg" class="img-fluid roundCorners dropShadow" alt="">
+    <div class="row d-flex justify-content-center spacer150T">
+      <div class="col-lg-8 col-md-12">
+        <img src="content/vacationMode/vacationMode.jpg" class="img-fluid roundCorners dropShadow" alt="">
+      </div>
+    </div>
+    <div class="row p-0 spacer150T">
+      <div class="col-12 p-0">
+        <img src="content/vacationMode/spotify.png" class="img-fluid dropStrongShadow roundCorners" alt="">
+      </div>
     </div>
   </div>
-  <div class="row spacer150T">
-    <div class="col-12">
-      <img src="content/vacationMode/spotify.png" class="img-fluid dropStrongShadow roundCorners" alt="">
-    </div>
-  </div>
+
+
   <!--div class="container mw-100 noPad" style="margin-top: 250px; position:absolute; bottom:0;"></div-->
-    <?php include'components/backButton.php' ?>
-    <?php include'components/contact.php' ?>
+  <?php include 'components/backButton.php' ?>
+  <?php include 'components/contact.php' ?>
 </body>
-<?php include'components/loader.php' ?>
+<?php include 'components/loader.php' ?>
