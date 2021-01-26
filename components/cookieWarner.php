7c819aaedef3eb2c43b908eff92b112d82058f33
Ole Walberg <greblavelo@gmail.com>
2021-01-26 21:23:52 +0100

    devtools better on small screens and removed jquery

--- components/cookieWarner.php
+++ components/cookieWarner.php
@@ -3,7 +3,7 @@
     <div class="row cookieWarning dropShadow color-bga roundCorners p-3">
       <div class="col-10">
         <h3 class="color-neg-fg">Cookies &#127850;</h3>
-        <p class="color-neg-fg m-0">Denne nettsiden bruker cookies for å forbedre brukeropplevelsen. Ingen personlig data brukes eller lagres.</p>
+        <p class="color-neg-fg m-0">Denne nettsiden bruker cookies for å forbedre brukeropplevelsen.</p>
       </div>
       <div class="col-2 d-flex align-items-center justify-content-end">
         <button id="acceptCookie" type="button" name="button" class="negButton">Ok</button>
