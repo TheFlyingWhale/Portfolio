28875d7aa63ccf804082edbb5a9fd90acbab797c
Ole Walberg <ole@studybud.no>
2022-01-17 20:05:39 +0100

    Change contact info

--- components/contact.php
+++ components/contact.php
@@ -1,11 +1,12 @@
 <div id="contactContainer" class="container viewBorder textWhite roundCorners p-5 mb-5 dropShadow">
   <div class="container noPad">
     <h1 class="textWhite aboutTitle">Ønsker du å ta en prat?</h1>
-    <p class="textWhite"> Kanskje har du noen gode idéer å dele? Eller ønsker du å ta en kaffe?
-        <br>
+    <p class="textWhite">
         Da er det bare å ta kontakt!
+        <br>
+        Send meg en melding om hvem du er og hva det gjelder så tar vi det der fra.
     </p>
-    <p class="textWhite"><span style="font-weight:600;">Mail:</span> mail@olewalberg.com</p>
-    <!--p class="textWhite"><span style="font-weight:600;">Tlf:</span> +47 957 60 985</p-->
+    <!-- <p class="textWhite"><span style="font-weight:600;">Mail:</span> mail@olewalberg.com</p> -->
+    <p class="textWhite"><span style="font-weight:600;">Tlf:</span> +47 957 60 985</p>
   </div>
 </div>
